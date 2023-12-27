3b5749364559a9cccdbef017f95787efa180ee64
skippybonez <108502188+skippybonez@users.noreply.github.com>
2023-12-27 15:16:57 -0500

    modified:   src/App.css 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
 
   return (
       <div>
+         <div className={"newRecord"}>
+          <button>New Loan</button>
+          
+        </div>
         <div className={"searchContainer"}>
           <button>Go</button>
           <input
@@ -13,7 +17,10 @@ function App() {
         </div>
   <div className="recordContainer">
     <div className="recordBlock">
-      <h1>Nigga</h1>
+      <h1>NiggaPenis</h1>
+    </div>
+    <div className="recordBlock">
+      <h1>NiggaPenis</h1>
     </div>
   </div>
       </div>
