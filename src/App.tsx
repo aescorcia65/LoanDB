208a098cd27a47f5073cbace28a81a9aeee5c074
Andrew Escorcia <drew.escorcia@gmail.com>
2023-12-27 17:05:39 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,28 @@ import './App.css';
 function App() {
 
 
-  return (
-      <div>
-         <div className={"newRecord"}>
-          <button>New Loan</button>
-          
+    return (
+        <div>
+            <div className={"top-left-button"}>
+                <button>New Loan</button>
+
+            </div>
+            <div className={"searchContainer"}>
+                <button>Go</button>
+                <input
+                    placeholder="Seach Loanee Name"
+                />
+            </div>
+            <div className="recordContainer">
+                <div className="recordBlock">
+                    <h1>testrecord</h1>
+                </div>
+                <div className="recordBlock">
+                    <h1>testrecord</h1>
+                </div>
+            </div>
         </div>
-        <div className={"searchContainer"}>
-          <button>Go</button>
-          <input
-              placeholder="Seach Loanee Name"
-          />
-        </div>
-  <div className="recordContainer">
-    <div className="recordBlock">
-      <h1>NiggaPenis</h1>
-    </div>
-    <div className="recordBlock">
-      <h1>NiggaPenis</h1>
-    </div>
-  </div>
-      </div>
-)
+    )
 }
 
 export default App;
