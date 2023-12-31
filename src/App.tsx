34cef56b76bf8d384b486032f8ac272d51eb4bc3
Andrew Escorcia <drew.escorcia@gmail.com>
2023-12-31 13:06:58 -0500

    filterpage

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage'; // import your components
 import NewRecord from './NewRecord';
 import RecordInfo from './RecordInfo';
+import FilterPage from "./FilterPage";
 
 function App() {
     return (
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<FilterPage />} />
           <Route path="/NewRecord" element={<NewRecord />} />
           <Route path="/RecordInfo" element={<RecordInfo />} />
 
---
+++ src/FilterPage.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const FilterPage: React.FC = () => {
+    const [names, setNames] = useState<string[]>([]);
+    const [selectedName, setSelectedName] = useState<string>('');
+
+    // Fetch names from the backend on component mount
+    useEffect(() => {
+        fetch('/api/names') // Replace with your API endpoint
+            .then(response => response.json())
+            .then(data => {
+                setNames(data.results);
+            })
+            .catch(error => console.error('Error fetching names:', error));
+    }, []);
+
+    // Handle dropdown change
+    const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedName(event.target.value);
+    };
+
+    return (
+        <div>
+            <select onChange={handleDropdownChange} value={selectedName}>
+                {names.map((name, index) => (
+                    <option key={index} value={name}>{name}</option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default FilterPage;
