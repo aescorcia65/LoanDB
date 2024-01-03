88d188a2a181b83b1bf1d90c1c13c7924fe54b6d
Aidan <810roldan@gmail.com>
2024-01-03 00:25:40 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/GridTest.jsx

--- src/GridTest.jsx
+++ src/GridTest.jsx
@@ -1,69 +1,34 @@
 import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-quartz.css';
-import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css'; // Core CSS
-import 'ag-grid-community/styles/ag-theme-quartz.css'; // Theme
 
-// Create new GridExample component
-const GridTest = () => {
-  // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState([
-    {
-      mission: 'Voyager',
-      company: 'NASA',
-      location: 'Cape Canaveral',
-      date: '1977-09-05',
-      rocket: 'Titan-Centaur ',
-      price: 86580000,
-      successful: true,
-    },
-    {
-      mission: 'Apollo 13',
-      company: 'NASA',
-      location: 'Kennedy Space Center',
-      date: '1970-04-11',
-      rocket: 'Saturn V',
-      price: 3750000,
-      successful: false,
-    },
-    {
-      mission: 'Falcon 9',
-      company: 'SpaceX',
-      location: 'Cape Canaveral',
-      date: '2015-12-22',
-      rocket: 'Falcon 9',
-      price: 9750000,
-      successful: true,
-    },
-  ]);
+import './HomePage.css'
 
-  // Column Definitions: Defines & controls grid columns.
-  const [colDefs, setColDefs] = useState([
-    { field: 'mission' },
-    { field: 'company' },
-    { field: 'location' },
-    { field: 'date' },
-    { field: 'price' },
-    { field: 'successful' },
-    { field: 'rocket' },
-  ]);
+function GridTest() {
+    const rowData = [
+        {LoanID: '216354', Name: 'Tony Meatballs', Principal: '$1224565', Due: '1000', Status: 'Y'},
+        {LoanID: '216123', Name: 'Joey Spaghetti', Principal: '$1462254', Due: '1000', Status: 'Y'},
+        {LoanID: '216634', Name: 'Chris Lasagna', Principal: '$02568', Due: '10a00', Status: 'Y'},
+        {LoanID: '216354', Name: 'Tony Meatballs', Principal: '$1225', Due: '1000', Status: 'Y'},
+        {LoanID: '216354', Name: 'Tony Meatballs', Principal: '$1225', Due: '1000', Status: 'Y'},
+        {LoanID: '216354', Name: 'Tony Meatballs', Principal: '$1225', Due: '1000', Status: 'Y'},
+    ];
 
-  // Container: Defines the grid's theme & dimensions.
-  return (
-    <div
-      className={
-        "ag-theme-quartz-dark"
-      }
-      style={{ width: '100%', height: '100%' }}
-    >
-      <AgGridReact rowData={rowData} columnDefs={colDefs} />
-    </div>
-  );
-};
+    const columnDefs = [
+        {field: 'LoanID'},
+        {field: 'Name'},
+        {field: 'Principal'},
+        {field: 'Due'},
+        {field: 'Status'}
+    ];
+    return (
+        <div className='ag-theme-alpine' style={{ height: 500 }}>
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={columnDefs}/>
+        </div>
+    );
+}
 
-// Render GridExample
-const root = createRoot(document.getElementById('root'));
-root.render(<GridTest />);
 export default GridTest;
